99c178a032e3ac68596494341d62b8dfe6e678fa
CraigJensen <cmjensen82@gmail.com>
2022-01-14 16:24:48 -0600

    completed tests for Intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require('../lib/Intern');
+
+test('will see if Intern has a school', () => {
+  const intern = new Intern('UT Coding Bootcamp');
+
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+test('if getSchool() will return a school', () => {
+  const intern = new Intern('UT Coding Bootcamp');
+
+  expect(intern.getSchool().school).toEqual(intern.school);
+});
+
+test('will return role of Intern', () => {
+  const intern = new Intern('UT Coding Bootcamp');
+
+  expect(intern.getRole().role).toBe('Intern');
+});
