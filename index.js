9020a5b42a63a71354d576458734de7fd4e05105
CraigJensen <cmjensen82@gmail.com>
2022-01-14 17:32:15 -0600

    manager questions done

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
-const Inquirer = require('inquirer');
+const inquirer = require('inquirer');
 
-const userInput = [
+const managerInput = [
   {
     type: 'input',
     name: 'name',
-    message: '',
+    message: "What is the Manager's name? (Required)",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Enter the Manager's name:");
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the Manager's ID number? (Required)",
+    validate: (idInput) => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Enter the Manager's ID number:");
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "What is the Manager's office number? (Required)",
+    validate: (officeInput) => {
+      if (officeInput) {
+        return true;
+      } else {
+        console.log('Enter an office number:');
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "what is the Manager's email",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log('Enter an email address:');
+      }
+    },
   },
 ];
+
+async function start() {
+  try {
+    const answers = await inquirer.prompt(managerInput);
+    console.log(answers);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
