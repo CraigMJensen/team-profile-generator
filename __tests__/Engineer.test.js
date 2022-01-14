21eba8ecf3558367433da5973754716dd842f325
CraigJensen <cmjensen82@gmail.com>
2022-01-14 16:13:34 -0600

    engineer tests completed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer');
+
+test('if engineer has github', () => {
+  const engineer = new Engineer('craigmjensen');
+
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('if getGithub function gets github object', () => {
+  const engineer = new Engineer('craigmjensen');
+
+  expect(engineer.getGithub().github).toEqual(engineer.github);
+});
+
+test('will return role of Engineer', () => {
+  const engineer = new Engineer('craigmjensen');
+
+  expect(engineer.getRole().role).toBe('Engineer');
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,13 @@
 const Manager = require('../lib/Manager');
+
+test('if officeNumber returns a number', () => {
+  const manager = new Manager(100);
+
+  expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('will return role of Manager', () => {
+  const manager = new Manager(100);
+
+  expect(manager.getRole().role).toBe('Manager');
+});
