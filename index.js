ad8716b31535de5517ce781ab9068a12c5e900d8
CraigJensen <cmjensen82@gmail.com>
2022-01-14 18:22:31 -0600

    added validation for numbers and email

--- index.js
+++ index.js
@@ -1,66 +1,73 @@
 const inquirer = require('inquirer');
 
-const managerInput = [
-  {
-    type: 'input',
-    name: 'name',
-    message: "What is the Manager's name? (Required)",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Enter the Manager's name:");
-        return false;
-      }
-    },
-  },
-  {
-    type: 'input',
-    name: 'id',
-    message: "What is the Manager's ID number? (Required)",
-    validate: (idInput) => {
-      if (idInput) {
-        return true;
-      } else {
-        console.log("Enter the Manager's ID number:");
-        return false;
-      }
-    },
-  },
-  {
-    type: 'input',
-    name: 'officeNumber',
-    message: "What is the Manager's office number? (Required)",
-    validate: (officeInput) => {
-      if (officeInput) {
-        return true;
-      } else {
-        console.log('Enter an office number:');
-        return false;
-      }
-    },
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: "what is the Manager's email",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log('Enter an email address:');
-      }
-    },
-  },
-];
+const teamArray = [];
 
-async function start() {
-  try {
-    const answers = await inquirer.prompt(managerInput);
-    console.log(answers);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const managerInput = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the Manager's name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Enter the Manager's name:");
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the Manager's ID number? (Required)",
+      default: () => {},
+      validate: function (id) {
+        valid = Number(id);
 
-start();
+        if (valid) {
+          return true;
+        } else {
+          console.log('Enter a number');
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is the Manager's office number? (Required)",
+      default: () => {},
+      validate: function (officeNum) {
+        valid = Number(officeNum);
+
+        if (valid) {
+          return true;
+        } else {
+          console.log('Enter a number');
+          return false;
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "what is the Manager's email",
+      default: () => {},
+      validate: function (email) {
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+        if (valid) {
+          return true;
+        } else {
+          console.log('Please enter a valid email');
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+managerInput().catch((error) => {
+  console.log(error);
+});
